3dc128adcb90dfccd511275577b67f894793671b
Jomat18 <manu.jose.torres@gmail.com>
2020-11-06 20:41:43 -0500

    feat: agregando el servidor en flask

---
+++ server.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+#dir = os.path.dirname(os.path.realpath(__file__))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/search", methods=["POST"])        
+def search():
+    data = request.json
+    words =  data["words"]
+    return jsonify(words)
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5000)
